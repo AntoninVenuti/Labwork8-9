f1bb0a662c4cf877c6ad9512c8bdd336366d8568
1902170 <1902170@laurea.fi>
2019-11-14 18:23:13 +0200

    create searchbar

--- src/app/songs/songs.page.ts
+++ src/app/songs/songs.page.ts
@@ -13,17 +13,29 @@ import { SongService } from '../services/song.service'; // Import SongService
   styleUrls: ['songs.page.scss']
 })
 export class SongsPage implements OnInit {
-  songs: Song[] = null;
+  songs: Song[] = [];
+  firebaseSongs: Song[] = null;
+  searchText: string = null;
 
   constructor(private songService: SongService) { }
 
   ngOnInit() {
     // subscribe to valueChanges methon in SongList in songService
     this.songService.get_SongList().valueChanges().subscribe(data => {
-      this.songs = []; // Initialize empty array
+      this.firebaseSongs = []; // Initialize empty array
       data.forEach(item => { // Loop items in collection
-        this.songs.push(item as Song); // Insert to array as song
+        this.firebaseSongs.push(item as Song); // Insert to array as song
       });
+      this.songs = this.firebaseSongs;
+
     });
+  }
+
+  search() {
+    // console.log(this.searchText);
+    if (this.searchText === null || this.firebaseSongs === null) {
+     return;
+    }
+    this.songs = this.firebaseSongs.filter(song => song.title.indexOf(this.searchText) >= 0);
   }
 }
